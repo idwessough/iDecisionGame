1555ad4fc61b9bf51af6aa07df1b5c4ae751249a
idwessough <sough.idwes@gmail.com>
2023-11-22 22:18:03 -0500

    Adding Button() Class with some objects

--- home_menu.py
+++ home_menu.py
@@ -34,11 +34,37 @@ def draw_text(text, font, color, surface, x, y):
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
+class Button:
+    def __init__(self, text, width, height, pos, elevation, action = None):
+        
+        # Core attributes
+        self.pressed = False
+        self.elevation = elevation
+        self.dynamic_elevation = elevation
+        self.original_y_pos = pos[1]
+        self.action = action
 
+        # Top rectangle 
+        self.top_rect = pygame.Rect(pos,(width,height))
+        self.top_color = '#475F77'
 
+        # Text
+        self.text_surf = font.render(text, True, '#FFFFFF')
+        self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
+
+        # Bottom rectangle
+        self.bottom_rect = pygame.Rect(pos,(width,height))
+        self.bottom_color = '#354B5E'
+        # Original method for button drawing
+        self.draw()
+ 
 
 # Main menu loop
-def main_menu(): 
+def main_menu():
+    play_button = Button('Play Game', 200, 40, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2), 5)
+    settings_button = Button('Settings', 200, 40, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 50), 5)
+    source_button = Button('Source Code', 200, 40, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 100), 5)
+    exit_button = Button('Exit', 200, 40, (SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 150), 5)
     background_image = pygame.image.load(os.path.join("resources", "Main_Menu_Background_Game_Image.png") )
     background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
